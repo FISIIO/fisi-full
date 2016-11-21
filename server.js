d67568f4c24fb2b14a956442cb3cad34bdcd6aae
Eric Krawczyk <eric@docnetwork.org>
2016-11-20 21:49:25 -0500

    server: actually serve static files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app     = express();
 const port    = process.env.PORT || 3000;
 
 // serve static frontend
-app.use(express.static('browser/dist'));
+app.use('/', express.static(__dirname + '/browser/dist'));
 
 // include api routes
 app.use('/api', require(__dirname + '/api'));
